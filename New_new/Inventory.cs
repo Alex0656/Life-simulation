6ed64ca9e68b5a28e6773f5422022f6a6bf48816
Aleksey Voistinov <waywwwwalex5@rambler.ru>
2024-01-08 23:35:05 +0700

    1. Некорректные комментарии. Недостатком этих комментариев можно назвать лишний шум в коде, который они создают. Этот шум создаёт сложности при чтении кода другими разработчиками, которые его разрабатывают и поддерживают. 2. Удаление комментариев. Данный метод рефакторинга позволяет сделать код мение награмождённым и повсить его читаемость. 3. Из-за того, что стало меньше лишнего/отвлекающего текста читаемость улучшилась.

--- New_new/Inventory.cs
+++ New_new/Inventory.cs
@@ -9,15 +9,11 @@ namespace New_new
     public class Inventory
     {
         public List<Resources> MyResources = new List<Resources>();
-        //private int _resources_limits = 25;
         public int _meat { get; set; } = 0;
         public int _fruit { get; set; } = 0;
         public int _plant { get; set; } = 0;
 
         private int _inventory_limits = 10;
-        //private int _meat_limits = 15;
-        //private int _fruit_limits = 25;
-        //private int _plant_limits = 15;
 
         public bool sum()
         {
@@ -41,83 +37,5 @@ namespace New_new
                 return false;
             }
         }
-
-        
-
-        /*
-        protected override void Walk()
-        {
-            if (inventory.CollectMeat())
-            {
-                FoodWalkMeat();
-            }
-            if (inventory.CollectFruit())
-            {
-                FoodWalkFruit();
-            }
-            if (inventory.CollectPlant())
-            {
-
-            }
-
-            else
-            {
-                int value = GameLogic.Random(0, 4);
-                if ((value == 1) && (_x < cols))
-                {
-                    _x = _x + 1;
-                }
-                else if ((value == 2) && (_y < row))
-                {
-                    _y = _y + 1;
-                }
-                else if ((value == 3) && (_x > 0))
-                {
-                    _x = _x - 1;
-                }
-                else if ((value == 4) && (_y > 0))
-                {
-                    _y = _y - 1;
-                }
-            }
-        }
-
-        
-
-        public bool CollectFruit()
-        {
-            if (_fruit < _fruit_limits)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        public bool CollectMeat()
-        {
-            if (_meat < _meat_limits)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        public bool CollectPlant()
-        {
-            if (_plant < _plant_limits)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-*/
     }
 }
